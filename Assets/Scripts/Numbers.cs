297f549c92368a5a78b3170456d3d0b8d04b76c7
Abdul-1421 <abdulrahman.alodaib2@gmail.com>
2025-07-12 14:49:53 +0300

    Assets & Scripts
    Palm Trees, movement, interactor

---
+++ Assets/Scripts/Numbers.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Numbers : MonoBehaviour, IInteractable
+{
+    public void Interact()
+    {
+        Debug.Log("It's a palm tree, who would have thunk");
+    }
+}
